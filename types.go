d233250b71a5d8fc9446ea8b5c1f198af30a39bb
Michael Gunkoff <m.onishenko@qvant.ru>
2021-03-04 16:47:17 +0300

    added Error and Warning types

--- types.go
+++ types.go
@@ -1,1 +1,14 @@
 package const_errs
+
+type Error string
+type Warning string
+
+// Error returns error description.
+func (err Error) Error() string {
+	return string(err)
+}
+
+// Error returns error description.
+func (err Warning) Error() string {
+	return string(err)
+}
